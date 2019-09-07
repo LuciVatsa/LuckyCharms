3a572edcece018442b174677cd977da6db345910
Balaji <balajiganeshoff@gmail.com>
2019-09-06 18:01:48 -0600

    update on example

--- Example1/game.js
+++ Example1/game.js
@@ -14,27 +14,38 @@ var config = {
             preload: preload,
             create: create,
             update: update
+            
         }
     };
 	var game = new Phaser.Game(config);
 	var keyA;
 	var keyD;
 	var keyS;
-	var keyW;
+    var keyW;
+    var player;
 function preload ()
 {
 	
        // this.load.image('logo', 'assets/sprites/phaser3-logo.png');
        // this.load.image('red', 'assets/particles/red.png');
        this.load.image('Monk', 'Cowboy_man.png');
-
+       this.load.image('sky','sky.png');
+        this.load.spritesheet('dude', 'dude.png',{ frameWidth:32, frameHeight:48});
 }
 
 function create ()
     {
+        this.add.image(400,300,'sky');
         this.add.image(100, 100, 'Monk');
-        this.drawKeyBoard();
+        player = this.physics.add.sprite(100,500,'dude');
+        this.createAnimation();
 
+        this.anims.create({
+            key:'left',
+            farmes: this.anims.generateFrameNumbers('dude',{start:0,end:3}),
+            frameRate:10,
+            repeat:-1
+        })
         keyW = this.input.keyboard.addkey(Phaser.Input.Keyboard.KeyCode.W);
 
         keyS = this.input.keyboard.addkey(Phaser.Input.Keyboard.KeyCode.S);
@@ -43,14 +54,14 @@ function create ()
         
     }
 
-function drawKeyBoard()
+function createAnimation()
 {
 
 }
 function update ()
 {
-	if(keyA.isDown)
+	if(keyD.isDown)
 	{
-		console.log('A');
+        player.anims.play('left',true);
 	}
 }
