828be141038801cdd38489a47addb90d111df160
LuciVatsa <saaranshvatsa@yahoo.in>
2019-09-10 11:15:45 -0600

    Update game.js

--- Example1/game.js
+++ Example1/game.js
@@ -202,29 +202,29 @@ function enemyMovement()
 
 
 
-       //enemy movement
-      //enemyMovement();
-//     var path = new Phaser.Curves.Path(50, 500);
+       enemy movement
+      enemyMovement();
+    var path = new Phaser.Curves.Path(50, 500);
 
-//     path.splineTo([ 164, 446, 274, 542, 412, 457, 522, 541, 664, 464 ]);
-//     path.lineTo(700, 300);
-//     path.lineTo(600, 350);
-//     path.ellipseTo(200, 100, 100, 250, false, 0);
-//     path.cubicBezierTo(222, 119, 308, 107, 208, 368);
-//     path.ellipseTo(60, 60, 0, 360, true);
+    path.splineTo([ 164, 446, 274, 542, 412, 457, 522, 541, 664, 464 ]);
+    path.lineTo(700, 300);
+    path.lineTo(600, 350);
+    path.ellipseTo(200, 100, 100, 250, false, 0);
+    path.cubicBezierTo(222, 119, 308, 107, 208, 368);
+    path.ellipseTo(60, 60, 0, 360, true);
 
-//     var graphics = this.add.graphics();
+    var graphics = this.add.graphics();
 
-//     //graphics.lineStyle(1, 0xffffff, 1);
+    //graphics.lineStyle(1, 0xffffff, 1);
 
-//     //path.draw(graphics, 128);
+    //path.draw(graphics, 128);
 
-//  enemy = this.add.follower(path, 50, 500, 'enemy');
+ enemy = this.add.follower(path, 50, 500, 'enemy');
 
-//     enemy.startFollow({
-//         duration: 10000,
-//         yoyo: true,
-//         repeat: -1,
-//         rotateToPath: true,
-//         verticalAdjust: true
-//     });
+    enemy.startFollow({
+        duration: 10000,
+        yoyo: true,
+        repeat: -1,
+        rotateToPath: true,
+        verticalAdjust: true
+    });
