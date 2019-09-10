15de6b0e72617d1cc304aec5de48b627c5833d50
Balaji <balajiganeshoff@gmail.com>
2019-09-10 11:06:33 -0600

    update.

--- Example1/game.js
+++ Example1/game.js
@@ -1,7 +1,7 @@
 
 var config = {
         type: Phaser.AUTO,
-        width: 600,
+        width: 608,
         height: 800,
         physics: {
             default: 'arcade',
@@ -20,7 +20,7 @@ var config = {
     };
 	
     var player;
-    var platform;
+    var slime;
     var cursors;
     var score = 0;
     var scoreText;
@@ -28,6 +28,7 @@ var config = {
     var X= 100;
     var Y= 500;
     var follower;
+    var portal;
 var path;
 var bounds;
 var graphics;
@@ -36,52 +37,29 @@ function preload ()
 {
 	
 
-       this.load.image('sky','sky.png');
-       this.load.image('platform', 'block.png');
-       this.load.image('enemy', 'Cowboy_man.png');
+       this.load.image('sky','background.png');
+       this.load.image('slime', 'slime.png');
+       this.load.image('enemy', 'enemySkeleton.png');
+       this.load.image('portal', 'tempPortal.png');
        //spirte loading
-       this.load.spritesheet('dude', 'dude.png',{ frameWidth:32, frameHeight:48});
+     //  this.load.spritesheet('dude', 'dude.png',{ frameWidth:32, frameHeight:48});
+     this.load.image('dude', 'Player.png');
 }
 
 function create ()
     {
         
-        this.add.image(400,300,'sky').setScale(1.7);
+        this.add.image(304,400,'sky');
         //enemy collision
         enemy= this.physics.add.group();
         enemy.create(100,600,'enemy');
         enemy.create(X,Y , 'enemy');
-//platform collision
-        platform =this.physics.add.group(); 
-        platform.create(100,300,'platform');
-        platform.create(300,500,'platform');
-       
-        //enemy movement
-      //enemyMovement();
-    // var path = new Phaser.Curves.Path(50, 500);
-
-    // path.splineTo([ 164, 446, 274, 542, 412, 457, 522, 541, 664, 464 ]);
-    // path.lineTo(700, 300);
-    // path.lineTo(600, 350);
-    // path.ellipseTo(200, 100, 100, 250, false, 0);
-    // path.cubicBezierTo(222, 119, 308, 107, 208, 368);
-    // path.ellipseTo(60, 60, 0, 360, true);
-
-    // var graphics = this.add.graphics();
-
-    // graphics.lineStyle(1, 0xffffff, 1);
-
-    // path.draw(graphics, 128);
-
-    // var lemming = this.add.follower(path, 50, 500, enemy);
-
-    // lemming.startFollow({
-    //     duration: 10000,
-    //     yoyo: true,
-    //     repeat: -1,
-    //     rotateToPath: true,
-    //     verticalAdjust: true
-    // });
+        //slime collision
+        slime =this.physics.add.group(); 
+        slime.create(100,300,'slime');
+        slime.create(300,500,'slime');
+       //
+ 
         // playercollision 
         player = this.physics.add.sprite(100,100,'dude');
         player.setCollideWorldBounds(true);
@@ -109,10 +87,11 @@ function create ()
         });
             cursors = this.input.keyboard.createCursorKeys();
         
-        this.physics.add.collider(player, platform);
-        this.physics.add.collider(player, platform, blockPush, null, null);
-        this.physics.add.overlap(enemy, platform, killEnemy, null , null);
-        //Collision with the player
+            this.physics.add.collider(player, slime);
+            //Collision with the player
+        this.physics.add.collider(player, slime, blockPush, null, null);
+        //Collision bet block and enemy
+        this.physics.add.overlap(enemy, slime, killEnemy, null , null);
         
     }
 
@@ -122,12 +101,12 @@ function update ()
     if(this.input.keyboard.checkDown(cursors.left, 250))
     {
         player.x -= 16;
-        player.anims.play('left', true);
+       // player.anims.play('left', true);
     }
     else if(this.input.keyboard.checkDown(cursors.right, 250))
     {
         player.x += 16;
-        player.anims.play('right', true);
+       // player.anims.play('right', true);
     }
    else if(this.input.keyboard.checkDown(cursors.up, 250))
     {
@@ -147,34 +126,34 @@ function update ()
 
 	
 }
-function blockPush(player , platform)
+function blockPush(player , slime)
 {
-    	 platform.setCollideWorldBounds(true);
+    	 slime.setCollideWorldBounds(true);
 
    var diff = 0;
    var maxDiff= 50;
-   if(player.x < platform.x )
+   if(player.x < slime.x )
     {
        
-        platform.setVelocityX(160);
+        slime.setVelocityX(160);
    }
-  else if(player.x > platform.x )
+  else if(player.x > slime.x )
   {
       
-        platform.setVelocityX(-160);
+        slime.setVelocityX(-160);
    }
-   else if(player.y < platform.y )
+   else if(player.y < slime.y )
     {
-      // platform.setActiveCollision();
-        platform.setVelocityY(160);
+      // slime.setActiveCollision();
+        slime.setVelocityY(160);
    }
-  else if(player.y > platform.y )
+  else if(player.y > slime.y )
   {
        
-        platform.setVelocityY(160);
+        slime.setVelocityY(160);
    }
 }
-function killEnemy(enemy, platform)
+function killEnemy(enemy, slime)
 {
  enemy.disableBody(true, true);
   //enemy.destory();
@@ -215,3 +194,37 @@ function enemyMovement()
         repeat: -1
     });
 }
+
+
+
+
+
+
+
+
+       //enemy movement
+      //enemyMovement();
+//     var path = new Phaser.Curves.Path(50, 500);
+
+//     path.splineTo([ 164, 446, 274, 542, 412, 457, 522, 541, 664, 464 ]);
+//     path.lineTo(700, 300);
+//     path.lineTo(600, 350);
+//     path.ellipseTo(200, 100, 100, 250, false, 0);
+//     path.cubicBezierTo(222, 119, 308, 107, 208, 368);
+//     path.ellipseTo(60, 60, 0, 360, true);
+
+//     var graphics = this.add.graphics();
+
+//     //graphics.lineStyle(1, 0xffffff, 1);
+
+//     //path.draw(graphics, 128);
+
+//  enemy = this.add.follower(path, 50, 500, 'enemy');
+
+//     enemy.startFollow({
+//         duration: 10000,
+//         yoyo: true,
+//         repeat: -1,
+//         rotateToPath: true,
+//         verticalAdjust: true
+//     });
