55c48006cd9e37cdca815f17cdd46cce1f013de8
Balaji <balaji.balaji.ganesh@gmail.com>
2019-09-11 19:50:39 -0600

    Portal match update

--- Example1/game.js
+++ Example1/game.js
@@ -5,7 +5,7 @@ var config = {
         height: 704,
         physics: {
             default: 'arcade',
-            
+
             arcade: {
                 gravity: { y: 0},
                 debug: true
@@ -15,11 +15,11 @@ var config = {
             preload: preload,
             create: create,
             update: update
-            
+
         }
     };
-	
-	
+
+
     var player;
     var slime;
     var cursors;
@@ -32,72 +32,90 @@ var config = {
     var portal;
     var isRight =-1;
     var isUp = -1;
+    var isRed = false;
+    var isBlue = false;
 var path;
 var bounds;
 var graphics;
 	 var game = new Phaser.Game(config);
 function preload ()
 {
-	
+
 
        this.load.image('sky','background.png');
        this.load.image('slime', 'trail.png');
        this.load.image('enemy', 'enemySkeleton.png');
-       this.load.image('portal', 'tempPortal.png');
+       //this.load.image('redPortal', 'Vortex-red.png');
        //spirte loading
-     //  this.load.spritesheet('dude', 'dude.png',{ frameWidth:32, frameHeight:48});
+      this.load.spritesheet('redPortal', 'Vortex-Red.png',{ frameWidth:96, frameHeight:64});
+      this.load.spritesheet('bluePortal', 'Vortex-Blue.png',{ frameWidth:96, frameHeight:64});
+      //player loading
      this.load.image('dude', 'Player.png');
 }
 
 function create ()
     {
     	//19 width boxes, 22 height boxes
-        
+
         this.add.image(304,400,'sky');
         //enemy collision
         enemy= this.physics.add.group();
         enemy.create(48,208,'enemy');
         enemy.create(240,608 , 'enemy');
-        //slime collision 
-        slime =this.physics.add.group(); 
+        //slime collision
+        slime =this.physics.add.group();
 
         slime.create(400,300,'slime').setCollideWorldBounds(true).setImmovable(true);
-        slime.create(100,400,'slime').setCollideWorldBounds(true).setImmovable(true);
+        slime.create(110,300,'slime').setCollideWorldBounds(true).setImmovable(true);
         slime.create(400,500,'slime').setCollideWorldBounds(true).setImmovable(true);
 
-       
-       //portal
-       portal = this.physics.add.staticGroup();
-       portal.create(400,400,'portal');
 
- 
-        // playercollision 
-        player = this.physics.add.sprite(400,100,'dude');
+
+
+        // playercollision
+        player = this.physics.add.sprite(400,200,'dude');
         player.setCollideWorldBounds(true);
         //player score text
        scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
-    
+       //animation for redPortal
         this.anims.create({
-            key: 'left',
-            frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-            frameRate: 10,
+            key: 'turnRed',
+            frames: this.anims.generateFrameNumbers('redPortal', { start: 1, end: 8 }),
+            frameRate: 15,
             repeat: -1
         });
-    
+        //animation for bluePortal
         this.anims.create({
-            key: 'turn',
-            frames: [ { key: 'dude', frame: 4 } ],
-            frameRate: 20
-        });
-    
-        this.anims.create({
-            key: 'right',
-            frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-            frameRate: 10,
+            key: 'turnBlue',
+            frames: this.anims.generateFrameNumbers('bluePortal', { start: 1, end: 8 }),
+            frameRate: 15,
             repeat: -1
         });
+
+        // this.anims.create({
+        //     key: 'turn',
+        //     frames: [ { key: 'dude', frame: 4 } ],
+        //     frameRate: 20
+        // });
+
+        // this.anims.create({
+        //     key: 'right',
+        //     frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        //     frameRate: 10,
+        //     repeat: -1
+        // });
             cursors = this.input.keyboard.createCursorKeys();
-        
+            //portal
+            portal = this.physics.add.staticGroup();
+
+            //redportal
+            portal.create(400,400,'redPortal').play('turnRed').setName('redPortal1');
+          portal.create(100,100,'redPortal').play('turnRed').setName('redPortal2');
+
+            //blueportal
+         portal.create(400,600,'bluePortal').play('turnBlue').setName('bluePortal1');
+           portal.create(100,500,'bluePortal').play('turnBlue').setName('bluePortal2');
+
             this.physics.add.collider(player, slime);
             //Collision with the player
         this.physics.add.collider(player, slime, blockPush, null, null);
@@ -105,52 +123,56 @@ function create ()
         this.physics.add.overlap(enemy, slime, killEnemy, null , null);
        this.physics.add.collider(slime , slime);
        this.physics.add.collider(slime, portal , portalShift ,null , null);
-        
+
+
+
+
     }
 
 function update ()
 {
-   
-   
+
+
+
     if(this.input.keyboard.checkDown(cursors.left, 250))
     {
-        player.x -= 16;
+        player.x -= 32;
        // player.anims.play('left', true);
     }
     else if(this.input.keyboard.checkDown(cursors.right, 250))
     {
-        player.x +=16;
+        player.x +=32;
        // player.anims.play('right', true);
     }
    else if(this.input.keyboard.checkDown(cursors.up, 250))
     {
-        player.y -= 16;
+        player.y -= 32;
     }
     else if(this.input.keyboard.checkDown(cursors.down, 250))
     {
-        player.y += 16;
+        player.y += 32;
     }
     else{
                 player.setVelocityX(0);
               player.setVelocityY(0);
                // player.anims.play('turn');
          }
-             
 
 
-	
+
+
 }
 
 //block movement
 function blockPush(player , slime)
 {
     	 slime.setImmovable(false);
-         
-   
+
+
    //left to right
     if(player.x < slime.x )
     {
-       
+
         slime.setVelocityX(160);
         slime.setVelocityY(0);
         isRight = 1;
@@ -159,7 +181,7 @@ function blockPush(player , slime)
    }
     else if(player.x > slime.x)
    {
-      
+
       slime.setVelocityX(-160);
       slime.setVelocityY(0);
       isRight = 0;
@@ -168,10 +190,9 @@ function blockPush(player , slime)
    //down to top
     else if(player.y < slime.y )
     {
-      
+
       // slime.setActiveCollision();
-      console.log(player.y);
-      console.log(isRight);
+
       slime.setVelocityY(160);
       slime.setVelocityX(0);
       isUp=  0;
@@ -180,13 +201,13 @@ function blockPush(player , slime)
    //up to down
     else if( player.y > slime.y)
   {
-  
+
         slime.setVelocityY(-160);
         slime.setVelocityX(0);
         isUp=  1;
         isRight = -1;
    }
-  
+
 }
 
 //Kill Function
@@ -194,17 +215,47 @@ function killEnemy(enemy, slime)
 {
  enemy.disableBody(true, true);
   //enemy.destory();
-  
+
      score += 10;
     scoreText.setText('Score: ' + score);
 }
 
 function portalShift(slime, portal)
 {
-    console.log(isRight);
+
+  if(portal.name == 'redPortal1' && isRed !=true  )
+  {
+
+    slime.x = 108;
+    slime.y = 108;
+
+    isRed = true;
+
+  }
+  else if(portal.name == 'redPortal2' && isRed !=true  && isBlue !=true)
+  {
+console.debug('asdasdasd')
+    slime.x = 400;
+    slime.y = 400;
+isRed = true;
+
+  }
+   if(portal.name == 'bluePortal1' && isBlue !=true)
+   {
+
      slime.x = 100;
-     slime.y = 100;
-     
+     slime.y = 500;
+     isBlue =true
+   }
+   else if(portal.name == 'bluePortal2' && isBlue !=true)
+    {
+
+      slime.x = 400;
+      slime.y = 600;
+         isBlue =true
+    }
+
+
      if(isRight == 1)
       {
         slime.setVelocityX(160);
@@ -214,7 +265,7 @@ function portalShift(slime, portal)
       else if(isRight==0){
         slime.setVelocityX(-160);
         slime.setVelocityY(0);
-        isRight = -1; 
+        isRight = -1;
       }
     if(isUp==1){
         slime.setVelocityY(-160);
@@ -225,10 +276,9 @@ function portalShift(slime, portal)
         slime.setVelocityY(160);
         slime.setVelocityX(0);
         isUp =-1;
+
     }
 
 
 
 }
-
-        
